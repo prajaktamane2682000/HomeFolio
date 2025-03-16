e962340793e6922affdf4e284fa3ea508a8498bf
Prajakta Mane <prajaktamane2682000@gmail.com>
2025-03-16 20:23:54 +0530

    Login and Register Fixed

--- components/navbar.jsx
+++ components/navbar.jsx
@@ -264,7 +264,11 @@ const NavBar = () => {
             )}
 
             {!session && (
-              <button className="flex items-center text-white bg-gray-700 hover:bg-gray-900 hover:text-white rounded-md px-3 py-2 my-5">
+              <button
+                key={index}
+                onClick={() => signIn(provider.id)}
+                className="flex items-center text-white bg-gray-700 hover:bg-gray-900 hover:text-white rounded-md px-3 py-2 my-5"
+              >
                 <i className="fa-brands fa-google mr-2"></i>
                 <span>Login or Register</span>
               </button>
